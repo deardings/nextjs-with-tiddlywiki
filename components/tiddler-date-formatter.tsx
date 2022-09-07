6d697b4d5ca46930298be43a59cabaac83730ad4
deardings <sne.ak@gmx.net>
2022-09-07 23:21:32 +0200

    Date format adjustments

--- components/tiddler-date-formatter.tsx
+++ components/tiddler-date-formatter.tsx
@@ -1,12 +1,16 @@
-import { parse, format } from 'date-fns'
+import { parse, format } from 'date-fns';
 
 type Props = {
-  dateString: string
-}
+  dateString: string;
+};
 
 const TiddlerDateFormatter = ({ dateString }: Props) => {
-  const date = parse(dateString, "yyyyMMddkkmmssSSS", new Date())
-  return <time dateTime={dateString}>{format(date, 'LLLL do, yyyy')}</time>
-}
+  const date = parse(dateString, 'yyyyMMddkkmmssSSS', new Date());
+  return (
+    <time dateTime={dateString}>
+      {format(date, "LLLL do, yyyy 'at' kk:mm")}
+    </time>
+  );
+};
 
-export default TiddlerDateFormatter
+export default TiddlerDateFormatter;
